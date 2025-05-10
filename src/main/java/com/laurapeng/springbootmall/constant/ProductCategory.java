9b664184ef8e331a58012658a2fb16790b85bab3
Laura Peng <laurapeng@LauradeMacBook-Pro.local>
2025-05-10 18:15:01 +0800

    Product Feature – refactor category, Enum

---
+++ src/main/java/com/laurapeng/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.laurapeng.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/laurapeng/springbootmall/model/Product.java
+++ src/main/java/com/laurapeng/springbootmall/model/Product.java
@@ -1,5 +1,6 @@
 package com.laurapeng.springbootmall.model;
 
+import com.laurapeng.springbootmall.constant.ProductCategory;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,7 +10,7 @@ public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
