bb1b29892eb412558ca01ea70d3c8d0d4f1d7620
Laura Peng <laurapeng@LauradeMacBook-Pro.local>
2025-05-14 18:21:35 +0800

    Product Feature – Implement product deletion

--- src/main/java/com/laurapeng/springbootmall/controller/ProductController.java
+++ src/main/java/com/laurapeng/springbootmall/controller/ProductController.java
@@ -52,4 +52,11 @@ public class ProductController {
 
         return ResponseEntity.ok(updateProduct);
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
